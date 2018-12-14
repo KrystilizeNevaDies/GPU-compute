f3918bbbd3eee65051c8b28fe2824032701f1596
Milan Košťák <milankostak@seznam.cz>
2018-12-14 17:25:51 +0100

    data shrinking test

--- src/main/Renderer.java
+++ src/main/Renderer.java
@@ -26,18 +26,19 @@ import static oglutils.ShaderUtils.COMPUTE_SHADER_SUPPORT_VERSION;
  */
 public class Renderer implements GLEventListener {
 
-    private int computeShaderProgram;
+    private int computeProgram, shrinkProgram;
     private int locOffset;
     private int[] locBuffer;
 
     private final int groupSize = 4;
-    private int size = (int) Math.pow(groupSize, 7);
+    private int size = 16;
     private int groupCount = size / groupSize;
     private final int dataSize = size * size;
     private IntBuffer data = IntBuffer.allocate(dataSize);
     private IntBuffer dataOut = Buffers.newDirectIntBuffer(dataSize);
+    private IntBuffer dataShrink = Buffers.newDirectIntBuffer(size);
 
-    private int offset = 4, compute = 0;
+    private int offset = 1, compute = 0;
     private int locSize;
 
     @Override
@@ -53,10 +54,11 @@ public class Renderer implements GLEventListener {
         glDrawable.setGL(new DebugGL4(glDrawable.getGL().getGL4()));
         GL4 gl = glDrawable.getGL().getGL4();
 
-        computeShaderProgram = ShaderUtils.loadProgram(gl, "/computeTest");
+        computeProgram = ShaderUtils.loadProgram(gl, "/computeTest");
+        shrinkProgram = ShaderUtils.loadProgram(gl, "/shrink");
 
-        locOffset = gl.glGetUniformLocation(computeShaderProgram, "offset");
-        locSize = gl.glGetUniformLocation(computeShaderProgram, "size");
+        locOffset = gl.glGetUniformLocation(computeProgram, "offset");
+        locSize = gl.glGetUniformLocation(computeProgram, "size");
 
         // buffer initialization
         data.rewind();
@@ -65,27 +67,32 @@ public class Renderer implements GLEventListener {
             data.put(i, r.nextInt(99));
         }
 
-        //System.out.print("Input Data values ");
-        //print(dataSize, data);
+        System.out.print("Input values");
+        print(dataSize, data);
 
         // declare and generate a buffer object name
-        locBuffer = new int[2];
-        gl.glGenBuffers(2, locBuffer, 0);
+        locBuffer = new int[3];
+        gl.glGenBuffers(3, locBuffer, 0);
 
         // bind the buffer and define its initial storage capacity
         gl.glBindBuffer(GL4.GL_SHADER_STORAGE_BUFFER, locBuffer[0]);
         gl.glBufferData(GL4.GL_SHADER_STORAGE_BUFFER, 4 * dataSize, data, GL4.GL_DYNAMIC_DRAW);
 
-        // bind the buffer and define its initial storage capacity
         gl.glBindBuffer(GL4.GL_SHADER_STORAGE_BUFFER, locBuffer[1]);
         gl.glBufferData(GL4.GL_SHADER_STORAGE_BUFFER, 4 * dataSize, dataOut, GL4.GL_DYNAMIC_DRAW);
+
+        gl.glBindBuffer(GL4.GL_SHADER_STORAGE_BUFFER, locBuffer[2]);
+        gl.glBufferData(GL4.GL_SHADER_STORAGE_BUFFER, 4 * size, dataShrink, GL4.GL_DYNAMIC_DRAW);
 
         // unbind the buffer
         gl.glBindBuffer(GL4.GL_SHADER_STORAGE_BUFFER, 0);
 
         //assign the index of shader storage block to the binding point (see shader)
-        gl.glShaderStorageBlockBinding(computeShaderProgram, 0, 0); //input buffer
-        gl.glShaderStorageBlockBinding(computeShaderProgram, 1, 1); //output buffer
+        gl.glShaderStorageBlockBinding(computeProgram, 0, 0); //input buffer
+        gl.glShaderStorageBlockBinding(computeProgram, 1, 1); //output buffer
+
+        gl.glShaderStorageBlockBinding(shrinkProgram, 0, 0); //shrink buffer
+        gl.glShaderStorageBlockBinding(shrinkProgram, 1, 1); //shrink buffer
     }
 
     private void print(final int dataSize, final IntBuffer data) {
@@ -118,27 +125,27 @@ public class Renderer implements GLEventListener {
         gl.glQueryCounter(timesBuffer.get(0), GL_TIMESTAMP);
 
         if (offset > 0) {
-            gl.glUseProgram(computeShaderProgram);
+            gl.glUseProgram(computeProgram);
 
             gl.glUniform1i(locOffset, offset);
             gl.glUniform1i(locSize, size);
 
             //set input and output buffer
-            if (compute % 2 == 0) {
+            //if (compute % 2 == 0) {
                 //bind input buffer
                 gl.glBindBuffer(GL4.GL_SHADER_STORAGE_BUFFER, locBuffer[0]);
                 gl.glBindBufferBase(GL4.GL_SHADER_STORAGE_BUFFER, 0, locBuffer[0]);
                 //bind output buffer
                 gl.glBindBuffer(GL4.GL_SHADER_STORAGE_BUFFER, locBuffer[1]);
                 gl.glBindBufferBase(GL4.GL_SHADER_STORAGE_BUFFER, 1, locBuffer[1]);
-            } else {
+            /*} else {
                 //bind input buffer
                 gl.glBindBuffer(GL4.GL_SHADER_STORAGE_BUFFER, locBuffer[1]);
                 gl.glBindBufferBase(GL4.GL_SHADER_STORAGE_BUFFER, 0, locBuffer[1]);
                 //bind output buffer
                 gl.glBindBuffer(GL4.GL_SHADER_STORAGE_BUFFER, locBuffer[0]);
                 gl.glBindBufferBase(GL4.GL_SHADER_STORAGE_BUFFER, 1, locBuffer[0]);
-            }
+            }*/
             gl.glQueryCounter(timesBuffer.get(1), GL_TIMESTAMP);
 
             gl.glDispatchCompute(size / groupSize, size / groupSize, 1);
@@ -158,9 +165,9 @@ public class Renderer implements GLEventListener {
             gl.glGetQueryObjectui64v(timesBuffer.get(2), GL_QUERY_RESULT, time3);
 
 
-            System.out.println(String.format("Time spent on the GPU 2-1 (bind): %f ms\n", (time2.get(0) - time1.get(0)) / 1000000.0));
-            System.out.println(String.format("Time spent on the GPU 3-2 (dispatch): %f ms\n", (time3.get(0) - time2.get(0)) / 1000000.0));
-            System.out.println(String.format("Time spent on the GPU 3-1 (all): %f ms\n", (time3.get(0) - time1.get(0)) / 1000000.0));
+            //System.out.println(String.format("Time spent on the GPU 2-1 (bind): %f ms", (time2.get(0) - time1.get(0)) / 1000000.0));
+            System.out.println(String.format("Time spent on the GPU 3-2 (dispatch): %f ms", (time3.get(0) - time2.get(0)) / 1000000.0));
+            //System.out.println(String.format("Time spent on the GPU 3-1 (all): %f ms", (time3.get(0) - time1.get(0)) / 1000000.0));
 
             // make sure writing to image has finished before read
             gl.glMemoryBarrier(GL4.GL_SHADER_STORAGE_BARRIER_BIT);
@@ -174,24 +181,44 @@ public class Renderer implements GLEventListener {
             gl.glGetBufferSubData(GL4.GL_SHADER_STORAGE_BUFFER, 0, 4 * dataSize, dataOut);
 
             System.out.println();
-            System.out.print("Output data values after iteration " + (compute + 1) + " offset " + offset);
+            System.out.print("Output values");
             dataOut.rewind();
-            //print(dataSize, dataOut);
+            print(dataSize, dataOut);
 
             //Buffers.newDirectIntBuffer()
 
+
+            gl.glUseProgram(shrinkProgram);
+
+            //bind input buffer
+            gl.glBindBuffer(GL4.GL_SHADER_STORAGE_BUFFER, locBuffer[1]);
+            gl.glBindBufferBase(GL4.GL_SHADER_STORAGE_BUFFER, 0, locBuffer[1]);
+            //bind output buffer
+            gl.glBindBuffer(GL4.GL_SHADER_STORAGE_BUFFER, locBuffer[2]);
+            gl.glBindBufferBase(GL4.GL_SHADER_STORAGE_BUFFER, 1, locBuffer[2]);
+
+
+            gl.glDispatchCompute(size / groupSize, size / groupSize, 1);
+
+
+            gl.glBindBuffer(GL4.GL_SHADER_STORAGE_BUFFER, locBuffer[2]);
+            gl.glGetBufferSubData(GL4.GL_SHADER_STORAGE_BUFFER, 0, 4 * size, dataShrink);
+            System.out.print("Shrink values");
+            dataShrink.rewind();
+            print(size, dataShrink);
+
             compute++;
             //offset = offset / 2;
-            offset = 0;
+            offset--;
 
         } else {
             System.exit(0);
         }
 
-        gl.glUseProgram(0);
+        /*gl.glUseProgram(0);
 
         gl.glClearColor(0.5f, 0.1f, 0.1f, 1.0f);
-        gl.glClear(GL4.GL_COLOR_BUFFER_BIT | GL4.GL_DEPTH_BUFFER_BIT);
+        gl.glClear(GL4.GL_COLOR_BUFFER_BIT | GL4.GL_DEPTH_BUFFER_BIT);*/
     }
 
     @Override
@@ -201,7 +228,7 @@ public class Renderer implements GLEventListener {
     @Override
     public void dispose(GLAutoDrawable glDrawable) {
         GL4 gl = glDrawable.getGL().getGL4();
-        gl.glDeleteProgram(computeShaderProgram);
+        gl.glDeleteProgram(computeProgram);
         gl.glDeleteBuffers(2, locBuffer, 0);
         gl.glGenBuffers(2, locBuffer, 0);
     }
